cf039cf43fe43676ceaa512e3757adff007bbd5e
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2024-11-22 10:45:49 -0500

    first commit

---
+++ assets/js/persons.js
@@ -0,0 +1,163 @@
+export const jobSeekers = [
+    {
+      Name: "Jeff Clement",
+      Email: "Jeff.clement61@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/jeffclement3",
+      Field: "Volunteer",
+      AdditionalInfo: "Job Seeker Volunteer"
+    },
+    {
+      Name: "Julia Rivard",
+      Email: "juliarivard10@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/julia-rivard/",
+      Field: "Web Design",
+      AdditionalInfo: "Seeking creative roles in website design and content creation"
+    },
+    {
+      Name: "Devang Naik",
+      Email: "devangnaik@outlook.com",
+      LinkedInUrl: "",
+      Field: "IT/Real Estate",
+      AdditionalInfo: "IT Professional/Enterprise Architect; Licensed Real Estate Agent"
+    },
+    {
+      Name: "Mike Choromanski",
+      Email: "Choromanski117@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/manski117",
+      Field: "Technology",
+      AdditionalInfo: ""
+    },
+    {
+      Name: "Arlene Quiles",
+      Email: "AQGJR@YAHOO.COM",
+      LinkedInUrl: "https://www.linkedin.com/in/arlene-quiles",
+      Field: "IT/Business Analysis",
+      AdditionalInfo: "Seeking Senior Business Analyst or Software Project Manager, 15+ years IT experience"
+    },
+    {
+      Name: "Mike Bersabal",
+      Email: "mbersabal1@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/michaelbersabal/",
+      Field: "Software/E-commerce",
+      AdditionalInfo: "Looking for jobs in software/technology and e-commerce"
+    },
+    {
+      Name: "Steve Jackson",
+      Email: "stevenljackson1@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/stevenljackson",
+      Field: "Software Engineering",
+      AdditionalInfo: "Seeking Director/VP of Software Engineering, experienced in multiple tech stacks"
+    },
+    {
+      Name: "Jason Lowry",
+      Email: "robert.jason.lowry@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/rlowry1",
+      Field: "Technology",
+      AdditionalInfo: ""
+    },
+    {
+      Name: "David Pazmino",
+      Email: "Pazmino.d@gmail.com",
+      LinkedInUrl: "",
+      Field: "Legal/Software",
+      AdditionalInfo: "12 years experience drafting software contracts"
+    },
+    {
+      Name: "Reina Lingle",
+      Email: "",
+      LinkedInUrl: "https://www.linkedin.com/in/reinalingle/",
+      Field: "Marketing Technology",
+      AdditionalInfo: "Generative AI Strategist, 15 years Marketing Technology experience"
+    },
+    {
+      Name: "Rachel Meeker",
+      Email: "470-695-0230",
+      LinkedInUrl: "https://www.linkedin.com/in/rachelmeeker/",
+      Field: "Data Analytics",
+      AdditionalInfo: "Seeking Senior Manager, Data Analytics & Reporting"
+    },
+    {
+      Name: "Brad Chicoine",
+      Email: "",
+      LinkedInUrl: "https://www.linkedin.com/in/bradchicoine/",
+      Field: "Marketing",
+      AdditionalInfo: "Performance and digital marketing executive"
+    },
+    {
+      Name: "Minh Diep",
+      Email: "mndiep@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/minh-n-diep/",
+      Field: "E-commerce/Customer Success",
+      AdditionalInfo: "Previously at Home Depot, 15 years in software, seeking eCommerce or Customer Success roles"
+    },
+    {
+      Name: "Leiza Cypriano Brown",
+      Email: "Leizacbrown@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/leizacypriano",
+      Field: "Project Management",
+      AdditionalInfo: "Senior Project Manager seeking new role"
+    },
+    {
+      Name: "Mark Vorko",
+      Email: "markvorko@gmail.com",
+      LinkedInUrl: "",
+      Field: "IT Management",
+      AdditionalInfo: "IT Leader, Director/Sr Mgr level"
+    },
+    {
+      Name: "Jeff Norton",
+      Email: "jeffnortonatl@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/jeff-norton-atl",
+      Field: "Software Engineering",
+      AdditionalInfo: "Seeking Director or Manager of Software Engineering"
+    },
+    {
+      Name: "William Melton",
+      Email: "william.melton@gmail.com",
+      LinkedInUrl: "https://www.linkedin.com/in/meltonw",
+      Field: "Sales",
+      AdditionalInfo: "Sales leader specializing in building sales organizations"
+    },
+    {
+      Name: "Lee Anthony",
+      Email: "Lanthony247@gmail.com",
+      LinkedInUrl: "",
+      Field: "Project Management/Data Analysis",
+      AdditionalInfo: "Project management, Data Analyst, 3D printing, AI explorer"
+    },
+    {
+      Name: "Del Winn",
+      Email: "dellwinn@gmail.com",
+      LinkedInUrl: "https://linkedin.com/in/delwinn",
+      Field: "Business Development/Law",
+      AdditionalInfo: "International & Domestic Business Development, Experienced Corporate Attorney"
+    },
+    {
+      Name: "Vandella Brown",
+      Email: "",
+      LinkedInUrl: "https://linkedin.com/in/vandella-brown-mba",
+      Field: "Unspecified",
+      AdditionalInfo: ""
+    },
+    {
+      Name: "Gordon Shepherd",
+      Email: "",
+      LinkedInUrl: "https://www.linkedin.com/in/gordon-shepherd816/",
+      Field: "Sales/Business Development",
+      AdditionalInfo: "Sales/business development, seeking more technical roles"
+    },
+    {
+        Name: "Sarah K Matta",
+        Email: "sarahkmatta@gmail.com",
+        LinkedInUrl: "https://www.linkedin.com/in/sarah-matta",
+        Field: "Software Engineering",
+        AdditionalInfo: "Full-stack Software Engineer heavy in .NET"
+    },
+    // {
+    //     Name: "",
+    //     Email: "",
+    //     LinkedInUrl: "https://www.linkedin.com/in/",
+    //     Field: "",
+    //     AdditionalInfo: ""
+    // }
+  ];
